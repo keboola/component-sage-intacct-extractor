ffd7447ddf0c0414149d2601e6648e7373ecba6d
Josef Kudera <46950237+kudj@users.noreply.github.com>
2026-03-17 09:19:36 +0100

    fix sync action

--- src/component.py
+++ src/component.py
@@ -161,7 +161,7 @@ class Component(ComponentBase):
     @sync_action("list_columns")
     def list_columns(self):
         # When called from within an endpoint array item, get the endpoint value from parameters
-        endpoint = self.configuration.parameters.get("source", {}).get("endpoint")
+        endpoint = self.cfg.source.endpoint
         if not endpoint:
             return []
 
